206a54ea83ee3486bf6192dd2962b2eefe95d38f
qrocafe1535 <matheusprobr@gmail.com>
2023-10-09 18:09:11 -0300

    commit

--- arjmaintool.sh
+++ arjmaintool.sh
@@ -809,6 +809,7 @@ take_screenshot() {
 
 desativa_recentes () {
 	gsettings set org.gnome.desktop.privacy remember-recent-files false
+	gsettings set org.gtk.Settings.FileChooser show-hidden false
 }
 
 # Função principal
