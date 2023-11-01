3f9b3b16818e754c3cad604954bf4e0564fafbd2
qrocafe1535 <matheusprobr@gmail.com>
2023-11-01 17:29:13 -0300

    commit

--- config/debian.sh
+++ config/debian.sh
@@ -8,6 +8,12 @@ instala_adw3 () { # habilita suporte a temas libadwaita trazendo melhora visual 
 	sleep 1
 }
 
+unattended-upgrade () { # habilitando security updates automáticos.
+	sudo apt install unattended-upgrade -y
+	sudo systemctl enable --now unattended-upgrade
+	echo -e "\n${VERDE}Habilitado Security Updates Automáticos com sucesso!${SEM_COR}\n"
+}
+
 cron_update_auto () { # automatiza update do systema
 	# exporta o comando para o arquivo em /etc/crontab
 	echo "0 9 * * * /usr/bin/apt update && /usr/bin/apt upgrade -y && /usr/bin/apt dist-upgrade -y && /usr/bin/apt autoremove -y " | sudo tee -a /etc/crontab
@@ -164,6 +170,7 @@ main_update_debian () {
 	misc
 	instala_wine
 	system_update
+	unattended-upgrade
 	suporte_flatpak
 	instala_adw3
 	instala_chrome
